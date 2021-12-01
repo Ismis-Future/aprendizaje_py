5b356cd7e17ec3dcd129d8dcdb1815b115f0a15a
Ismis-Future <herreraismael469@gmail.com>
2021-11-30 19:16:23 -0500

    actualizacion 3

---
+++ list.py
@@ -0,0 +1,60 @@
+demo_list=[1, "hola mundo", True]
+colors = ['red', 'green', 'vilet']
+numbers_list= list((1,2,4,6,7,8.9))
+
+# r=list(range(1 , 10)) #range es para crear lista desde los valores que le digas
+# print(r)
+
+# print(len(numbers_list))
+# print(numbers_list[-5])
+# print('1' in numbers_list)#imprimir si lo que le dijiste esta e una variable
+
+# print(colors)
+# colors[2] = 'yellow' #cambiar el valor de una lista
+# print(colors)
+
+#colors.append('violet') #cambiar el valor
+#colors.append(('azul', 'yellow')) 
+#colors.extend(['azul', 'yellow']) #extender valores basado en la lista que e des
+
+#colors.insert(1, 'yellow') #insertar un nuevo valor en la pocision que le indiques
+
+#colors.insert(len(colors), 'yellow')
+
+#print(colors)
+
+#colors.pop() #Eliminar un valor
+
+#colors.remove('green') #quitar un valor en especifico
+
+#colors.clear() #limpiar todo lo de la lista
+
+#colors.sort(reverse=True) #ordenar alfabeticamente y con reverse=True al reversa 
+
+
+
+
+
+
+
+print(colors.index('green'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#con pop solo puedes eliminar un indice o si no el ultimo
+#con remove quito el valor que yo le de por el nombre
+#con clear limpio todo
+#con index saber el indice
+#con cout contar
