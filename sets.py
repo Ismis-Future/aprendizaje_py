ecf47c0651c97a1fde058f927464c1f6af9d7c65
Ismis-Future <79584395+Ismis-Future@users.noreply.github.com>
2021-12-07 10:30:09 -0500

    actualización 6

---
+++ sets.py
@@ -0,0 +1,13 @@
+colors = {'red' , 'green', 'blue'}
+
+print(type(colors))
+
+print('red' in colors)
+
+colors.add('violet') #agregar u elemento
+print(colors)
+colors.remove('green')
+print(colors)
+
+colors.clear()
+print(colors)
--- tuples.py
+++ tuples.py
@@ -1,4 +1,8 @@
-x = (1,2,3,) #Las tuplas no soportan ambios
+x = (1,2,3,5) #Las tuplas no soportan ambios
 
-del x #eliminar una tupla
-print(x)
+#del x #eliminar una tupla
+#print(x)
+locations = {
+    (35.777 , 50.08):"Tokio"
+    (13315, 65415380):"España"
+}
