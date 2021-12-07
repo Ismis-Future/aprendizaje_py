531f5b48739b6cfed42b252b3185fe40edf242fb
Ismis-Future <79584395+Ismis-Future@users.noreply.github.com>
2021-12-07 17:40:24 -0500

    Actualización 8

--- condicionales.py
+++ condicionales.py
@@ -1,3 +1,23 @@
-x = input("que edad tienes: ")
-int(x)
-print(f"la edad que tienes es: {x}")
+#x = int(input("di un número del 1 -30: "))
+
+#if x < 30: 
+    #print("x es menor que 30")
+#elif x > 30:   
+    #print("x es mayor que 30")
+#elif x == 30:
+    #print("x es igual a 30")
+
+
+# color = "blue"
+
+# if color == "red":
+#     print("El color es rojo")
+# elif color == "blue":
+#     print("El color es blue")
+# else:
+#     print("El color es verde")
+
+x = 5
+
+if x > 2 and x <= 10:
+    print("x es un numero mayor que 2 7 menor que 10")
