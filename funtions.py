5eebe825c1c21053c2ed574025a1e482a078a26b
Ismis-Future <79584395+Ismis-Future@users.noreply.github.com>
2021-12-08 16:44:56 -0500

    Actualización 10

---
+++ funtions.py
@@ -0,0 +1,19 @@
+# def hello(): # palabra clave def indica que es una definición de función
+#     print(f"tu número favorito es el {x}")
+
+# x = int(input("cual es tu número favorito: "))
+
+# if x <= 5:
+#     hello()
+# elif x >= 5:
+#     print(f"No conozco ese número")
+
+
+#return lo que le digamos
+#dentro del parantecis de una función puedo pasrle un paramatro
+
+add = lambda n1, n2: n1 +n2
+
+print(add(10 ,40))
+
+#lambda es para dar un nombre anonimo
