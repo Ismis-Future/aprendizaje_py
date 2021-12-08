d7b676676c842ac3c3825e2fa9db902ef91c117b
Ismis-Future <79584395+Ismis-Future@users.noreply.github.com>
2021-12-08 16:22:38 -0500

    Actualización 9

---
+++ loops.py
@@ -0,0 +1,18 @@
+# foods = ['apples', 'bread', 'cheese', 'milk', 'bananas']
+
+# for food in foods:
+#     if food == 'cheese':
+#         print("compara eso")
+#     print(food) 
+
+# for numbre in "hello":
+#     print(numbre)
+
+#break es para romper el proceso
+#continue continuar
+
+count = 4
+
+while count <= 10:
+    print(count)
+    count = count + 1
