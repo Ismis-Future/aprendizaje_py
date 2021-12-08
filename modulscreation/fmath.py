b9a0164a976c6503535f92fb3746bf1bb6936717
Ismis-Future <79584395+Ismis-Future@users.noreply.github.com>
2021-12-08 17:06:29 -0500

    Actualzacion de la funciones

---
+++ modulscreation/fmath.py
@@ -0,0 +1,12 @@
+#Los modulos so funciones que nos agilizan el trabajo al escrit codigo
+
+def suma(n1, n2): #Se crea la funcion
+    print(n1 + n2)
+
+def resta(n1, n2):
+    print(n1 - n2)
+
+
+
+
+#Ahora en mates.py vas a ver como los importas
---
+++ modulscreation/mates.py
@@ -0,0 +1,4 @@
+import fmath #se ha importado el archivo craeado
+
+fmath.suma(1,50) #se le llama al modulo fmath y luego de eso usamos la funcion anteriormente creada
+fmath.resta(30, 80)
