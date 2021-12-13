842aaf027d4c7f60dcf648fe26e187b383e3ed24
Ismis-Future <79584395+Ismis-Future@users.noreply.github.com>
2021-12-13 16:37:13 -0500

    tk actualizacion #1

--- Tkinter/images.py
+++ Tkinter/images.py
@@ -1,12 +1,20 @@
 #como insertar una imagen
 
 import tkinter as tk
+from tkinter import *
+from tkinter.constants import RIGHT
 
 ventana = tk.Tk()
 ventana.title('Holas')
 
 
-image_tk = tk.PhotoImage(file='Tkinter/logo_CV.png') #con PhotoImage podemos insertar una imagen, con file indicamos la direccion o la imagen que querramos poner
+image_tk = tk.PhotoImage(file='Tkinter/logo.png') #con PhotoImage podemos insertar una imagen, con file indicamos la direccion o la imagen que querramos poner
 
+
+label = tk.Label(ventana, image=image_tk).pack()
+
+#side es para mover a la derecha cuando le cambiamos de tamaño
+
+lblMensaje = tk.Label(ventana, image=image_tk).pack(side= 'right')
 
 ventana.mainloop()
