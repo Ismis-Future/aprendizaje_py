af4132fbcec4254d698682e420ffb90b5bcd5d96
Ismis-Future <79584395+Ismis-Future@users.noreply.github.com>
2021-12-13 17:04:49 -0500

    Movimntos de arhivos

--- Tkinter/images.py
+++ Tkinter/images.py
@@ -8,7 +8,7 @@ ventana = tk.Tk()
 ventana.title('Holas')
 
 
-image_tk = tk.PhotoImage(file='Tkinter/logo.png') #con PhotoImage podemos insertar una imagen, con file indicamos la direccion o la imagen que querramos poner
+image_tk = tk.PhotoImage(file='img/logo.png') #con PhotoImage podemos insertar una imagen, con file indicamos la direccion o la imagen que querramos poner
 
 
 label = tk.Label(ventana, image=image_tk).pack()
@@ -20,9 +20,10 @@ Hola a todos yo soy ismael y estoy
 aprendiendo tkinter
 """
 
-lblMensaje = tk.Label(ventana, image=image_tk).pack(side= 'right')
+lblMensaje3 = tk.Label(ventana, text=mensaje, image=image_tk, compound=tk.CENTER).pack()
+#lblMensaje = tk.Label(ventana, image=image_tk).pack(side= 'right')
 
-lblMensaje = tk.Label(ventana, image=image_tk).pack(side= 'left')
+#lblMensaje2 = tk.Label(ventana, image=image_tk).pack(side= 'left')
 
 
 ventana.mainloop()
